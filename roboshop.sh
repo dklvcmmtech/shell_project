85b421c9ef1e4d74efddca8bcd4d8d6667f3453e
Chandra Mouly <dklvcm.mtech@gmail.com>
2026-04-03 05:19:34 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -13,7 +13,7 @@ for instance in ${INSTANCES_TEMP[@]}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
-    if [ $instance != 'frontend']
+    if [ $instance != 'frontend' ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
